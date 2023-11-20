0be4e57a4e464968c08766524f273a3acf6dea9b
Armando Terrazas Gómez <A01640924@tec.mx>
2023-11-20 16:34:01 -0600

    Add Firestore integration and display animal data

--- lib/src/pages/home_screen.dart
+++ lib/src/pages/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:the_vet_app/src/routes/routes.dart';
 
@@ -8,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Screen', style: TextStyle(color: Colors.white)),
+        title: const Text('My animals', style: TextStyle(color: Colors.white)),
         actions: [
           IconButton(
             icon: const Icon(Icons.person),
@@ -19,8 +20,33 @@ class HomeScreen extends StatelessWidget {
         ],
         backgroundColor: Colors.deepPurple,
       ),
-      body: const Center(
-        child: Text('Home Screen'),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('animals').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return ListView.builder(
+            itemCount: snapshot.data?.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot data = snapshot.data!.docs[index];
+              return Card(
+                child: ListTile(
+                  title: Text(data['name']),
+                  subtitle: Text(data['age']),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.deepPurple,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
     );
   }
